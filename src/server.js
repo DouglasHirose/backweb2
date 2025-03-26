7fd7c05bfa04074e19e196ee19269b57b80ecc4b
Your NameDouglas <k.japa@outlook.com>
2025-03-25 22:28:52 -0300

    aula 25.03.2025

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@
 import express from "express"
 import user_router from "./routers/user_router.js"
 import product_router from "./routers/product_router.js"
+import cors from 'cors';
 
 const app = express()
 const port = 3000
 
 // Middleware
+app.use(cors())
 app.use(express.json()) // faz o parse do json e transforma em objeto no req.body
 
 app.use("/user", user_router)
